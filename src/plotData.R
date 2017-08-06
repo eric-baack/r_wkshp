1f5a1f8525eaff8824106596e2cf66d8e0297182
Eric-Baack <baacer01@luther.edu>
2017-08-06 11:10:59 -0700

    modify to include another comment

--- src/plotData.R
+++ src/plotData.R
@@ -1,3 +1,7 @@
-pdf(file = '../results/firstHistogram.pdf')
+#comment
+# another comment
+whales <- bbmroData
+setwd("c:/Users/baacer01/r_wkshp/results")
+pdf(file = 'firstHistogram.pdf')
 hist(whales$SST)
 dev.off()
